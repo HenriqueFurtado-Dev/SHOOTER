be0daf18d82ed11a59f4f5536bb5d9b5eeb57f2b
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2023-03-20 18:56:49 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var shoot = document.querySelector("body")
+var ammo = document.querySelector(".ammo-cont")
+var ammunitation = 30
+var audio = document.querySelector("audio")
+var gun = document.querySelector(".gun")
+var target = document.querySelector(".target")
+var points = document.querySelector(".points")
+
+
+function shooter() {
+    console.log("atirou!")
+    ammunitation--
+    ammo.innerHTML = ammunitation
+    audio.play()
+    audio.currentTime = 0
+    if(gun.classList.contains("shooting")) {
+        gun.classList.remove("shooting")
+    } else {
+        gun.classList.add("shooting")
+    }
+}
+
+let point = 0
+
+function win() {
+    lista = [
+        800,
+        200,
+        500,
+        400,
+        600,
+        100
+    ]
+
+    let randomNum = lista[Math.floor(Math.random() * lista.length )]
+    console.log(randomNum)
+
+    x = 0
+    target.style.left = `${randomNum}px`
+    target.style.top = `${randomNum}px`
+    
+    
+    point++
+    points.innerHTML = point
+
+}
+
+function reload() {
+    var reload = document.querySelector("#reload")
+    ammo.innerHTML = 30
+    reload.play()
+}
